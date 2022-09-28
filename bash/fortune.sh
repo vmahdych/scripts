7e246a736e428cadda3fc267fdbea4217fe6bd05
Vitalii Mahdych <vitalii.mahdych@gmail.com>
2022-09-28 16:55:10 +0300

    Move bash scripts to the separate folder and create folder for python scripts

---
+++ bash/fortune.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Program to tell a persons fortune
+
+echo -e "\n~~ Fortune Teller ~~\n"
+
+RESPONSES=("Yes" "Sure;)" "No" "No way!" "Maybe" "Outlook good" "Don't count on it" "Ask again later" "Oh! It's a tough one")
+N=$(( RANDOM % 9 ))
+
+function GET_FORTUNE() {
+  if [[ ! $1 ]]
+  then
+    echo Ask a yes or no question:
+  else
+    echo Try again. Make sure it ends with a question mark:
+  fi
+
+  read QUESTION
+}
+
+GET_FORTUNE
+
+until [[ $QUESTION =~ \?$ ]]
+do
+  GET_FORTUNE again
+done
+
+echo -e "\n${RESPONSES[$N]}"
---
+++ python/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+x=input('give me number:')
+
+if int(x) > 10 :
+    print('huge number')
+else :
+    print('that`s a small one')
+
+print('Done!')
